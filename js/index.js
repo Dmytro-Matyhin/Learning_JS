a046434e2c5fab5fe21462a951a76bda15c8367c
Dmytro <dima.mattock@gmail.com>
2020-08-28 09:44:34 +0300

    Lesson 2

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let firstName = prompt('What is your name?', 'name')
-let lastname = prompt('What is your last name?', 'last name')
-let age = prompt('what is your age?', 'age')
-alert(`
-  Пользователь: ${firstName} ${lastname} 
-  Возраст: ${age}
-  `)
---
+++ js/lesson2.js
@@ -0,0 +1,116 @@
+//FIRST TASK
+
+{
+  let firstNum = +prompt('Write first num?', 0)
+  let secondNum = +prompt('Write second num', 0)
+  let digit = prompt('Write digit?')
+
+  let result;
+
+  if (typeof(firstNum) !== 'number' || typeof(secondNum) !== 'number') {
+    result = 'error'
+    alert(result)
+  }
+
+  switch (digit) {
+    case '+': {
+      result = firstNum + secondNum
+      alert(result)
+    };
+    break;
+    case '-': {
+      result = firstNum - secondNum
+      alert(result)
+    };
+    break;
+    case '*': {
+      result = firstNum * secondNum
+      alert(result)
+    };
+    break;
+    case '/': {
+      result = firstNum / secondNum
+      alert(result)
+    };
+    break;
+  }
+
+  switch (false) {
+    case typeof(firstNum) !== 'number':
+    case typeof(secondNum) !== 'number': 
+    case firstNum === '':
+    case secondNum === '':
+    case digit === '': {
+      result = 'error'
+      alert(result)
+    }
+  }
+}
+
+
+// SECOND TASK
+
+
+{
+  let num = +prompt('Write a number?', 0)
+
+  let result = 0;
+
+  for (let i = 0; i < num; i++) {
+    if (num <= 10) continue;
+    result += i;
+  }
+  alert(result)
+}
+
+
+// THIRD TASK
+
+
+{
+  function check(n) {
+    if(typeof(n) === 'number') {
+      return 1
+    } else if (typeof(n) === 'string') {
+      return 0
+    } else return -1
+  }
+  
+  console.log(check(1))
+  console.log(check('Hello'))
+  console.log(check(null))
+}
+
+
+// ADDITIONAL TASKS
+
+
+{
+  const COLOR = 'green'
+  let count = 0;
+
+  while(count < 3) {
+    let divine = prompt('What is the color?')
+    if (divine === COLOR) {
+      alert('You win!')
+      break;
+    } else if (count === 2) {
+      alert('Good luck for the next time')
+    } else alert('Try again')
+      count++;
+  }
+}
+
+
+
+{
+  function substringExists(str, substr) {
+    if (str.indexOf(substr) != -1) {
+      return true
+    } else return false
+  }
+  
+  console.log(substringExists('Я люблю круассаны' ,'люблю'))
+  console.log(substringExists('Он кушает мороженное' ,'хлеб'))
+}
+
